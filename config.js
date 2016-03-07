fe4a085babeb756fbd2d24fe778cfa2209a70423
liubo <liubo@boqii.net>
2016-03-07 14:04:10 +0800

    develop

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
   qiniu: {
     host: '7xq22v.com2.z0.glb.qiniucdn.com',
     bucket_name: 'h5-images',
-    pre_path: 'svnm/boqiivet20160305/production',
+    pre_path: 'svnm/boqiivet20160305/develop',
     ACCESS_KEY: 'sYZeTVmmEEyVkWKeKf4sqnVlGNhIcAk_zGAEUqgO',
     SECRET_KEY: '_TLO3Huiv64jRIf_HRu5oE9DhnzQSflCByGMrn-Q',
     upload_dir: [
